eb7a93ee9bae898f83122985744db767f09f6665
ambar <srivastavaa@student.unimelb.edu.au>
2019-04-06 04:16:59 +0100

    changes

--- main.m
+++ main.m
@@ -16,41 +16,58 @@ packetHandler();
 motor1_ID = 1; 
 
 motor1 = XL320(motor1_ID, port_num); 
-% motor1.torqueEnable;
-
-%% Routine
-motor1.torqueDisable;
-motor1.jointMode;
 motor1.torqueEnable;
 
-dxl_present_position = motor1.getPos;
-fprintf('PresPos:%03d\n', typecast(uint32(dxl_present_position), 'int32'));
-motor1.setPos(500);
-dxl_present_position = motor1.getPos;
-fprintf('PresPos:%03d\n', typecast(uint32(dxl_present_position), 'int32'));
-pause(2);
-motor1.setPos(700);
-dxl_present_position = motor1.getPos;
-fprintf('PresPos:%03d\n', typecast(uint32(dxl_present_position), 'int32'));
-pause(2);
+%% Routine
+
+speed1 = 800;
+speed2 = 500;
+speed3 = 1523;
+
+targetSpeed = [];
+measuredSpeed = [];
+time = [];
 
 motor1.torqueDisable;
 motor1.wheelMode;
 motor1.torqueEnable;
 
-dxl_present_speed = motor1.getSpeed;
-fprintf('PresSpeed:%03d\n', typecast(uint32(dxl_present_speed), 'int32'));
-motor1.setSpeed(1000);
-dxl_present_speed = motor1.getSpeed;
-fprintf('PresSpeed:%03d\n', typecast(uint32(dxl_present_speed), 'int32'));
-pause(2);
-motor1.setSpeed(0);
-pause(2);
-motor1.setSpeed(2000);
-pause(2);
-motor1.setSpeed(0);
-dxl_present_speed = motor1.getSpeed;
-fprintf('PresSpeed:%03d\n', typecast(uint32(dxl_present_speed), 'int32'));
+motor1.setSpeed(speed1);
+% tic
+% while toc < 15
+%     targetSpeed = [targetSpeed speed1];
+%     measuredSpeed = [measuredSpeed motor1.getSpeed];
+%     time = [time toc];
+% end
+% motor1.setSpeed(speed2);
+% while toc < 10
+%     targetSpeed = [targetSpeed speed2];
+%     measuredSpeed = [measuredSpeed motor1.getSpeed];
+%     time = [time toc];
+% end
+% motor1.setSpeed(speed3);
+% while toc < 15
+%     targetSpeed = [targetSpeed speed3];
+%     measuredSpeed = [measuredSpeed motor1.getSpeed];
+%     time = [time toc];
+% end
+
+
+    
+
+% dxl_present_speed = motor1.getSpeed;
+% fprintf('PresSpeed:%03d\n', typecast(uint32(dxl_present_speed), 'int32'));
+% motor1.setSpeed(1000);
+% dxl_present_speed = motor1.getSpeed;
+% fprintf('PresSpeed:%03d\n', typecast(uint32(dxl_present_speed), 'int32'));
+% pause(2);
+% motor1.setSpeed(0);
+% pause(2);
+% motor1.setSpeed(2000);
+% pause(2);
+% motor1.setSpeed(0);
+% dxl_present_speed = motor1.getSpeed;
+% fprintf('PresSpeed:%03d\n', typecast(uint32(dxl_present_speed), 'int32'));
 
 
 
@@ -63,9 +80,6 @@ closePort(port_num);
 
 % Unload Library
 unloadlibrary(lib_name);
-
-close all;
-clear all;
 
 
 
