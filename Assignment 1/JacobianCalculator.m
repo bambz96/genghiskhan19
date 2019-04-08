16f0885a1a711b65da5dece4d1cb58aa43cc3543
William Ellett <williamjellett@gmail.com>
2019-04-08 20:52:38 +1000

    First part of the Jacobian Calculator

---
+++ Assignment 1/JacobianCalculator.m
@@ -0,0 +1,22 @@
+%{ 
+Author: Assignment Group 20
+William Ellett 586703
+
+Calculation of Robot Jacobian. 
+%}
+
+%Unit vector z in reference frame
+z0 = [0;0;1];
+
+
+
+
+
+
+
+
+function Ji = jacobianRevolute(zi, Pi)
+  Ji = zeros(6, 1); %initialize jacobian
+  Ji(1:3) = cross(zi, Pi);
+  Ji(3:6) = zi; 
+end
---
+++ Assignment 1/jacobianCol.m
@@ -0,0 +1,14 @@
+%{ 
+Author: Assignment Group 20
+William Ellett 586703
+
+Function to calculate Jacobian Column
+ie: partial derivatives for one generalised velocity
+
+%}
+
+function Ji = jacobianCol(zi, Pi)
+    
+
+
+end
