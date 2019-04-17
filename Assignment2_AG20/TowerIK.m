e41432ab9d4fb89e9bd1919a0dc0b1a64e40d8b5
William Ellett <williamjellett@gmail.com>
2019-04-17 11:26:07 +1000

    I think this is just the final commit from assignment2

---
+++ Assignment2_AG20/TowerIK.m
@@ -0,0 +1,46 @@
+%{
+ Tests inerse Kinematics on veriticies of tower
+ Quick little hack job...
+%}
+%% Set location of Tower
+x_t = 200;
+y_t = 0;
+z_t = 0;
+theta = 0;
+
+z_upper = 270;
+
+ZRotation_Matrix = [cosd(theta) sind(theta) 0; -sind(theta) cosd(theta) 0; 0 0 1];
+
+f0_ro_p = [x_t ; y_t ; z_t];
+f1_rp_c1 = [-37.5; 37.5; 0];
+f1_rp_c2 = [-37.5; -37.5; 0];
+f1_rp_c3 = [37.5; -37.5; 0];
+f1_rp_c4 = [37.5; 37.5; 0];
+
+
+c1 = f0_ro_p+ ZRotation_Matrix*f1_rp_c1;
+c2 = f0_ro_p+ ZRotation_Matrix*f1_rp_c2;
+c3 = f0_ro_p+ ZRotation_Matrix*f1_rp_c3;
+c4 = f0_ro_p+ ZRotation_Matrix*f1_rp_c4;
+
+
+c5 = c1 + [0;0;z_upper];
+c6 = c2 + [0;0;z_upper];
+c7 = c3 + [0;0;z_upper];
+c8 = c4 + [0;0;z_upper];
+
+verticies = [c1, c2, c3, c4, c5, c6, c7, c8];
+
+%% Make and test robot
+
+Jenghis = robot;
+i = 1;
+Q_sol = zeros(8,5);
+
+for v = verticies
+    [q1,q2,q3,q4,q5] = Jenghis.inverseKinematics.findQ(v(1), v(2), v(3), 0);
+    Q_sol(i,:) = [q1,q2,q3,q4,q5];
+    i = i + 1;
+    Jenghis.drawPose(q1,q2,q3,q4,q5);
+end
