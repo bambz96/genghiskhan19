45a8cf256b3237f80f4af1ab0452461c339ad483
Ambar Tablet <a.srivastava@murmotorsports.com>
2019-05-21 23:02:59 +1000

    Slight GUI updates

--- jBlock.m
+++ jBlock.m
@@ -15,9 +15,9 @@ classdef jBlock < handle
         
         approachX = 4; % used in calculating approach (block widths)
         approachZ = 2; % used in calculating approach (block hieghts)
-        dropHeight = 0.008; % height to drop the block from
+        dropHeight = 0.005; % height to drop the block from
 
-        dropIncrement = 0.0004 % scale drop height with z position
+        dropIncrement = 0.0005 % scale drop height with z position
     end
     
     properties(Access = private)
